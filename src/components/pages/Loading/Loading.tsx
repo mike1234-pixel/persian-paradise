60856f0901bc5126146d691b06fbb47f67d16eaa
Mike <miketandy@protonmail.com>
2023-08-06 17:48:48 +0100

    update loading screen

--- src/components/pages/Loading/Loading.tsx
+++ src/components/pages/Loading/Loading.tsx
@@ -1,11 +1,22 @@
-import { Content } from "antd/es/layout/layout"
-import { Skeleton } from "antd"
+import { Typography } from "antd"
 import styles from "./Loading.module.css"
 
 export const Loading = () => {
+  const numBubbles = 10
+  const bubbles = Array.from({ length: numBubbles }, (_, index) => (
+    <span
+      key={index}
+      className={styles.bubbleContainer}
+      style={{ "--i": index + 1 } as React.CSSProperties}
+    >
+      <span className={styles.bubble}></span>
+    </span>
+  ))
+
   return (
-    <Content className={styles.root}>
-      <Skeleton active />
-    </Content>
+    <div className={styles.wrapper}>
+      {bubbles}
+      <Typography.Title level={1}>loading...</Typography.Title>
+    </div>
   )
 }
