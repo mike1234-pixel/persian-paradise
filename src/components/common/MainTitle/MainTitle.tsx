9a68ff34d361f3447594a718bf3709b5fc1e0643
Mike <miketandy@protonmail.com>
2024-03-24 20:52:30 +0000

    remove unneeded component

--- src/components/common/MainTitle/MainTitle.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './MainTitle.module.css'
-
-export const MainTitle = () => {
-  return (
-    <div className={styles.root}>
-      <h1>Persian Paradise</h1>
-    </div>
-  )
-}
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,13 +1,12 @@
 import { Button } from 'antd'
 import { Link } from 'react-router-dom'
-import { MainTitle } from '../../common/MainTitle/MainTitle'
 import styles from './Home.module.css'
 
 export const Home = () => {
   return (
     <div className={styles.root}>
       <div className={styles.text}>
-        <MainTitle />
+        <h1>Persian Paradise</h1>
         <p className={styles.subText}>
           A simple entry point into learning Farsi for English speakers
         </p>
