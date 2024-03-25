1d0f32cbf3ba84393fdcb7c8c0e130198280bc34
Mike <miketandy@protonmail.com>
2024-03-25 18:45:18 +0000

    add unit test

---
+++ src/components/common/Confetti/Confetti.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { ConfettiEffect } from './Confetti'
+import { ConfettiAnimationContext } from '../../../context/ConfettiAnimationContext'
+
+describe('ConfettiEffect component', () => {
+  it('should not render if renderConfetti is false', () => {
+    render(
+      <ConfettiAnimationContext.Provider
+        value={{
+          renderConfetti: false,
+          releaseTheConfetti: jest.fn,
+          confettiPieces: 0,
+          setConfettiPieces: jest.fn
+        }}
+      >
+        <ConfettiEffect />
+      </ConfettiAnimationContext.Provider>
+    )
+
+    expect(screen.queryByTestId('confetti')).not.toBeInTheDocument()
+  })
+
+  it('should render the correct number of confetti pieces', () => {
+    const confettiPieces = 100
+
+    const confetti = render(
+      <ConfettiAnimationContext.Provider
+        value={{
+          renderConfetti: true,
+          releaseTheConfetti: jest.fn,
+          confettiPieces,
+          setConfettiPieces: jest.fn
+        }}
+      >
+        <ConfettiEffect />
+      </ConfettiAnimationContext.Provider>
+    )
+
+    expect(confetti.getByTestId(confettiPieces)).toBeInTheDocument()
+  })
+})
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+global.HTMLCanvasElement.prototype.getContext = () => {
+  return {
+    clearRect: jest.fn()
+  } as any
+}
