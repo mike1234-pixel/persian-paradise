e7f19d08c9101727be41e2165f1de5f3ad48ebb1
Mike <miketandy@protonmail.com>
2024-04-21 22:55:28 +0100

    try official cyprus gh action

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,5 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: 'cypress/support/e2e.ts'
-  },
-  env: {
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL ?? 'http://localhost:4000',
-    REACT_APP_BASE_URL:
-      process.env.REACT_APP_BASE_URL ?? 'http://localhost:3000'
   }
 })
