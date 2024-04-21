c8dec7141e840f349776e1f77a3a96b731f1b90a
Mike <miketandy@protonmail.com>
2024-04-21 17:18:18 +0100

    setup mocks

--- cypress/e2e/1-basic-journey/initial-load.cy.ts
+++ cypress/e2e/1-basic-journey/initial-load.cy.ts
@@ -1,8 +1,18 @@
+import phrases from '../../fixtures/1-phrases.json'
+import food from '../../fixtures/2-food.json'
 /// <reference types="cypress" />
 
 describe('initial load', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/modules'
+      },
+      [phrases, food]
+    ).as('modules')
   })
 
   it('initially displays a loading screen', () => {
