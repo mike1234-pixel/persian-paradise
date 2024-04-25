bf988595823ff677a9611e3fc6dc91ffc8950b54
Mike <miketandy@protonmail.com>
2024-04-25 20:47:09 +0100

    replace loading animation with lottie animation

--- src/components/pages/Loading/Loading.test.tsx
+++ src/components/pages/Loading/Loading.test.tsx
@@ -2,20 +2,8 @@ import { render } from '@testing-library/react'
 import { Loading } from './Loading'
 
 describe('<Loading />', () => {
-  it('renders the correct number of bubbles', () => {
-    const { container } = render(<Loading />)
-    const bubbles = container.querySelectorAll('.bubbleContainer')
-    expect(bubbles.length).toBe(10)
-  })
-
   it('renders the loading text', () => {
     const { getByText } = render(<Loading />)
     expect(getByText('loading...')).toBeInTheDocument()
-  })
-
-  it('renders a Typography.Title component', () => {
-    const { container } = render(<Loading />)
-    const title = container.querySelector('.ant-typography')
-    expect(title).toBeInTheDocument()
   })
 })
--- src/components/pages/Loading/Loading.tsx
+++ src/components/pages/Loading/Loading.tsx
@@ -1,23 +1,14 @@
+import Lottie from 'lottie-react'
 import { Typography } from 'antd'
 import { Content } from 'antd/es/layout/layout'
+import loadingAnimation from '../../../assets/animations/loadingAnimation.json'
 import styles from './Loading.module.css'
 
 export const Loading = () => {
-  const numBubbles = 10
-  const bubbles = Array.from({ length: numBubbles }, (_, index) => (
-    <span
-      key={index}
-      className={styles.bubbleContainer}
-      style={{ '--i': index + 1 } as React.CSSProperties}
-    >
-      <span className={styles.bubble}></span>
-    </span>
-  ))
-
   return (
     <Content>
       <div className={styles.wrapper}>
-        {bubbles}
+        <Lottie animationData={loadingAnimation} loop={true} />
         <Typography.Title level={1}>loading...</Typography.Title>
       </div>
     </Content>
