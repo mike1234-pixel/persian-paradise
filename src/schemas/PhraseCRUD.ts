e781664014633eefdc8c0037758cf27248274e9e
Mike <miketandy@protonmail.com>
2024-05-07 21:58:20 +0100

    replace zod with yup

--- src/schemas/PhraseCRUD.ts
+++ src/schemas/PhraseCRUD.ts
@@ -1,27 +1,27 @@
-import z from 'zod'
+import * as yup from 'yup'
 
-export const RegistersSchema = z.object({
-  informal: z.string(),
-  formal: z.string()
+export const RegistersSchema = yup.object().shape({
+  informal: yup.string(),
+  formal: yup.string()
 })
 
-export const PhraseSchema = z.object({
-  en: z.string(),
-  fa: z.array(z.string()).or(RegistersSchema),
-  emoji: z.string().optional(),
-  hint: z.string().optional()
+export const PhraseSchema = yup.object().shape({
+  en: yup.string().required(),
+  fa: yup.array().of(yup.string().required()).required(),
+  emoji: yup.string().optional(),
+  hint: yup.string().optional()
 })
 
-export const CourseModuleSchema = z.object({
-  title: z.string(),
-  subtitle: z.string().optional(),
-  emoji: z.string().optional(),
-  phrases: z.array(PhraseSchema)
+export const CourseModuleSchema = yup.object().shape({
+  title: yup.string(),
+  subtitle: yup.string().optional(),
+  emoji: yup.string().optional(),
+  phrases: yup.array().of(PhraseSchema)
 })
 
-export const ModulesListSchema = z.array(CourseModuleSchema)
+export const ModulesListSchema = yup.array().of(CourseModuleSchema)
 
-export type RegistersModel = z.infer<typeof RegistersSchema>
-export type PhraseModel = z.infer<typeof PhraseSchema>
-export type CourseModuleModel = z.infer<typeof CourseModuleSchema>
-export type ModulesListModel = z.infer<typeof ModulesListSchema>
+export type RegistersModel = yup.InferType<typeof RegistersSchema>
+export type PhraseModel = yup.InferType<typeof PhraseSchema>
+export type CourseModuleModel = yup.InferType<typeof CourseModuleSchema>
+export type ModulesListModel = yup.InferType<typeof ModulesListSchema>
